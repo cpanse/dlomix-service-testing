11d1e4c797f9f674e6fe2d0024982d1723974ca3
Christian Panse <cp@fgcz.ethz.ch>
2023-04-15 11:22:26 +0200

    Add parallel

--- runme.bash
+++ runme.bash
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-curl -s https://fgcz-ms.uzh.ch/fasta/fgcz_10090_UP00589_cnl_20230405.fasta \
-  | fcat \
-  | head -n 1000 \
-  | while read i;
-  do
-    echo 
-    echo $i \
-    | tryptic-digest \
+
+## https://gitlab.bfabric.org/proteomics/prx/-/tree/master/src
+
+function digestandquery(){
+    echo $1 \
+    | ~/bin/tryptic-digest \
     | egrep "^[A-Z]{8,30}$" \
     | python3 main.py
-  done
+
+}
+
+
+export -f digestandquery
+
+curl -s https://fgcz-ms.uzh.ch/fasta/fgcz_10090_UP00589_cnl_20230405.fasta \
+  | fcat \
+  | head -n 10000 \
+  | parallel -j $1 digestandquery 
