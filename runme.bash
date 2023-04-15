e16d3964ff9ae41135ea7709840c95d2b9dfde1f
Christian Panse <cp@fgcz.ethz.ch>
2023-04-15 10:55:37 +0200

    Script cosmetics

--- runme.bash
+++ runme.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-curl -q https://fgcz-ms.uzh.ch/fasta/fgcz_10090_UP00589_cnl_20230405.fasta \
+curl -s https://fgcz-ms.uzh.ch/fasta/fgcz_10090_UP00589_cnl_20230405.fasta \
   | fcat \
+  | head -n 1000 \
   | while read i;
   do
     echo 
