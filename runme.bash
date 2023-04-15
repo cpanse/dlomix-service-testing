099062dff0a2914b24cfd1791ea4bc8c7dfdf3f7
Christian Panse <cpanse@users.noreply.github.com>
2023-04-15 11:22:50 +0200

    Update runme.bash

--- runme.bash
+++ runme.bash
@@ -17,4 +17,4 @@ export -f digestandquery
 curl -s https://fgcz-ms.uzh.ch/fasta/fgcz_10090_UP00589_cnl_20230405.fasta \
   | fcat \
   | head -n 10000 \
-  | parallel -j $1 digestandquery 
+  | parallel -j 16 digestandquery 
